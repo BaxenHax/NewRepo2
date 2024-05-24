de6e1194b926a807c17ec187ae078d83cdf4017d
Rodrigo Sartori <rodrigo.sartori1544@gmail.com>
2024-05-23 22:18:21 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Exercicios_Uninove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInteiros = new System.Windows.Forms.Button();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnReais = new System.Windows.Forms.Button();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInteiros
+            // 
+            this.btnInteiros.Location = new System.Drawing.Point(463, 60);
+            this.btnInteiros.Name = "btnInteiros";
+            this.btnInteiros.Size = new System.Drawing.Size(75, 23);
+            this.btnInteiros.TabIndex = 0;
+            this.btnInteiros.Text = "Inteiros";
+            this.btnInteiros.UseVisualStyleBackColor = true;
+            this.btnInteiros.Click += new System.EventHandler(this.btnInteiros_Click);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(316, 70);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(14, 13);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "A";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(357, 63);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 2;
+            // 
+            // btnReais
+            // 
+            this.btnReais.Location = new System.Drawing.Point(463, 144);
+            this.btnReais.Name = "btnReais";
+            this.btnReais.Size = new System.Drawing.Size(75, 23);
+            this.btnReais.TabIndex = 3;
+            this.btnReais.Text = "Reais";
+            this.btnReais.UseVisualStyleBackColor = true;
+            this.btnReais.Click += new System.EventHandler(this.btnReais_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(316, 154);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(14, 13);
+            this.lblB.TabIndex = 4;
+            this.lblB.Text = "B";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(357, 147);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(357, 196);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.btnReais);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.btnInteiros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInteiros;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnReais;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios_Uninove
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReais_Click(object sender, EventArgs e)
+        {
+            double a, b, soma;
+            /* Entrada de dados*/
+            a = Convert.ToDouble(txtA.Text);
+            b = double.Parse(txtB.Text);
+
+            /* Processamento */
+            soma = a + b;
+
+            /* Saída de dados */
+            txtResultado.Text = soma.ToString();
+
+        }
+
+        private void btnInteiros_Click(object sender, EventArgs e)
+        {
+            double a, b, soma;
+
+            /*Entrada de dados */
+             a = Int32.Parse(txtA.Text);
+             b = Int32.Parse(txtB.Text);
+
+            soma = a + b;
+            txtResultado.Text = soma.ToString();
+        }
+    }
+}
